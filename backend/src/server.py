5c114218de87524aecc873c6dfa4e7db01a7ab94
andrewzho <andrewzho2003@gmail.com>
2024-10-19 10:34:24 -0700

    updating the server code

--- backend/src/server.py
+++ backend/src/server.py
@@ -158,7 +158,7 @@ async def delete_item(item_id: str, request: Request):
 # Main function to start the server
 def main(argv=sys.argv[1:]):
     try:
-        uvicorn.run("server:app", host="0.0.0.0", port=3001, reload=DEBUG)
+        uvicorn.run("server:app", host="127.0.0.1", port=3001, reload=DEBUG)
     except KeyboardInterrupt:
         pass
 
