58b6f4f011de36ef1b8cd040aed952ca1c267dd5
tyeon <tyeon@uci.edu>
2024-10-19 03:33:26 -0700

    init agent api boilerplate

---
+++ backend/api/agent.py
@@ -0,0 +1,12 @@
+import fastapi
+
+router = fastapi.APIRouter()
+
+@router.get("/")
+def get_root():
+    return "Hello from Agent API"
+
+# TODO: Get itinerary from LLM agent with post bucket list data
+@router.post("/itinerary")
+async def get_itinerary():
+    pass
