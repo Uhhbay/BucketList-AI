350acd323d0c09cd80538faf9ed7bafc76876130
johntrinhvu <johntrinhvu@yahoo.com>
2024-10-19 10:34:09 -0700

    change d

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -16,9 +16,9 @@ export default function Dashboard() {
         <div className="pt-28 min-h-screen">
             <div className="flex flex-col items-center justify-center">
                 <h1 className="text-3xl sm:text-5xl font-semibold">Your BucketList</h1>
-                <div className="mt-4 bg-gray-300 flex flex-col rounded-lg">
+                <div className="mt-4 bg-gray-300 flex flex-col rounded-lg max-w-md w-11/12 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-4xl">
                     <form onSubmit={handleSubmit}>
-                        <div className="flex">
+                        <div className="flex justify-between">
                             <div className="input ml-2">
                                 <input 
                                     type="text"
@@ -29,7 +29,7 @@ export default function Dashboard() {
                                     className="text-xs"
                                 />
                             </div>
-                            <button type="submit" className="submit-button ml-5 p-3 bg-sky-600 mr-2 mt-2 text-white rounded-md">Add to list</button>
+                            <button type="submit" className="submit-button p-3 bg-sky-600 mr-2 mt-2 text-white rounded-md">Add to list</button>
                         </div>
                     </form>
                     <ul className="m-4">
