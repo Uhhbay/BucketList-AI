912f38fa147aa26ae5ef8a0d99cf3e211a36c384
johntrinhvu <johntrinhvu@yahoo.com>
2024-10-19 12:08:02 -0700

    make dash look nice

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (inputValue.trim() != '') {
+        if (inputValue.trim() !== '') {
             setItems([...items, inputValue]);
             setInputValue('');
         }
@@ -16,25 +16,30 @@ export default function Dashboard() {
         <div className="pt-28 min-h-screen">
             <div className="flex flex-col items-center justify-center">
                 <h1 className="text-3xl sm:text-5xl font-semibold">Your BucketList</h1>
-                <div className="mt-4 bg-gray-300 flex flex-col rounded-lg max-w-md w-11/12 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-4xl">
+                <div className="mt-4 flex flex-col rounded-lg max-w-md w-11/12 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-4xl">
                     <form onSubmit={handleSubmit}>
-                        <div className="flex justify-between">
-                            <div className="input ml-2">
-                                <input 
-                                    type="text"
-                                    required
-                                    value={inputValue}
-                                    onChange={(e) => setInputValue(e.target.value)}
-                                    placeholder="Something you'd like to do"
-                                    className="text-xs"
-                                />
-                            </div>
-                            <button type="submit" className="submit-button p-3 bg-sky-600 mr-2 mt-2 text-white rounded-md">Add to list</button>
+                        <div className="flex items-center p-2 gap-2">
+                            <input 
+                                type="text"
+                                required
+                                value={inputValue}
+                                onChange={(e) => setInputValue(e.target.value)}
+                                placeholder="Something you'd like to do"
+                                className="shadow-md text-sm flex-grow p-2 border rounded-md focus:outline-none"
+                            />
+                            <button type="submit" className="shadow-md p-2 bg-sky-600 text-white rounded-md hover:bg-sky-400 ease-in duration-100">Add to List</button>
                         </div>
                     </form>
-                    <ul className="m-4">
+                    <div className="m-3 flex gap-6 font-semibold border-b-2 border-gray-600">
+                        <h3>No.</h3>
+                        <h3>Bucket Item</h3>
+                    </div>
+                    <ul className="p-2">
                         {items.map((item, index) => (
-                            <li className="" key={index}>{item}</li>
+                            <li className="flex gap-6 items-center" key={index}>
+                                <span className="w-8 text-center">{index + 1}</span>
+                                <span className="flex-grow">{item}</span>
+                            </li>
                         ))}
                     </ul>
 
